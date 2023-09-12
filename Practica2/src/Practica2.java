83f6594154558c129b6c023f672ec90a9e59a0bf
Santiago Reyes <santiago_reyes@ciencias.unam.mx>
2023-09-11 19:37:12 -0600

    Modificamos el codigo para que no haya bloques de codigo repetidos

--- Practica2/src/Practica2.java
+++ Practica2/src/Practica2.java
@@ -31,96 +31,29 @@ public class Practica2 {
             System.out.println("Se produjo un error al cargar la imagen introducida");
         }
 
-        /* Este bloque de codigo crea una nueva imagen nI con las mismas caracteristicas de la imagen i, y 
-         * la modifica pixel por pixel para que nI ahora este en escala de grises.
+        /* Este bloque de codigo crea una nuevas imagenes nI, nI2, nI3 y nI4 con las mismas caracteristicas de la imagen i, y 
+         * las modifica pixel por pixel para aplicarles filtros (escala de grises, verde, rojo y tonos sepia respectivamente)
          * 
-         * Crea una nueva imagen nI exactamente con las mismas caracteristicas de i, utiliza ciclos anidados para recorrer pixel por 
-         * pixel la imagen y asignarle un nuevo valor rgb en escalas de grises. Por ultimo guarda la imagen creada nI
-         * en un nuevo archivo formato jpg llamado i1.jpg en el directorio imagenes.
+         * Utiliza ciclos anidados para recorrer pixel por pixel las imagenes y les asigna nuevos valores rgb. Por ultimo guarda las imagenes 
+         * creadas en nuevos archivos formato jpg en el directorio imagenes.
          */
         
         BufferedImage nI = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        Color c;
+        BufferedImage nI2 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage nI3 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage nI4 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        Color c, c2, c3, c4;
+        int c1, c2a, c3a, c11, c22, c33;
+
         for (int w = 0; w < i.getHeight(); w++) {
             for (int z = 0; z < i.getWidth(); z++) {
                 c = new Color(i.getRGB(z, w));
-                p = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
-                nI.setRGB(z, w, new Color(p, p, p).getRGB());
-            }
-        }
-
-        try {
-            ImageIO.write(nI, "jpg", new File("imagenes/i1.jpg"));
-            System.out.println("La imagen nI se ha guardado correctamente en i1.jpg");
-        } catch (Exception e) {
-            System.err.println("Ocurrio un error al intentar guardar la informacion de la imagen creada nI en el nuevo archivo i1.jpg");
-        }
-
-        /* Este bloque de codigo crea una nueva imagen nI2 con las mismas caracteristicas de la imagen i, y 
-         * la modifica pixel por pixel para que nI2 ahora este en escala de rojos. 
-         * 
-         * Crea una nueva imagen nI2 exactamente con las mismas caracteristicas de i, utiliza ciclos anidados para recorrer pixel por 
-         * pixel la imagen y asignarle un nuevo valor rgb en escalas de rojos. Por ultimo guarda la imagen creada nI2
-         * en un nuevo archivo formato jpg llamado i2.jpg en el directorio imagenes.
-         */
-
-        BufferedImage nI2 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        Color c2;
-        for (int w = 0; w < i.getHeight(); w++) {
-            for (int z = 0; z < i.getWidth(); z++) {
                 c2 = new Color(i.getRGB(z, w));
-                nI2.setRGB(z, w, new Color(c2.getRed(), 0, 0).getRGB());
-            }
-        }
-
-        try {
-            ImageIO.write(nI2, "jpg", new File("imagenes/i2.jpg"));
-            System.out.println("La imagen nI2 se ha guardado correctamente en i2.jpg");
-        } catch (Exception e) {
-            System.err.println("Ocurrio un error al intentar guardar la informacion de la imagen creada nI2 en el nuevo archivo i2.jpg");
-        }
-
-        /* Este bloque de codigo crea una nueva imagen nI3 con las mismas caracteristicas de la imagen i, y 
-         * la modifica pixel por pixel para que nI3 ahora este en escala de verdes. 
-         * 
-         * Crea una nueva imagen nI3 exactamente con las mismas caracteristicas de i, utiliza ciclos anidados para recorrer pixel por 
-         * pixel la imagen y asignarle un nuevo valor rgb en escalas de verdes. Por ultimo guarda la imagen creada nI3
-         * en un nuevo archivo formato jpg llamado i3.jpg en el directorio imagenes.
-         */
-
-        BufferedImage nI3 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        Color c3;
-        for (int w = 0; w < i.getHeight(); w++) {
-            for (int z = 0; z < i.getWidth(); z++) {
                 c3 = new Color(i.getRGB(z, w));
-                nI3.setRGB(z, w, new Color(0, c3.getGreen(), 0).getRGB());
-            }
-        }
+                c4 = new Color(i.getRGB(z, w));
+                p = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
 
-        try {
-            ImageIO.write(nI3, "jpg", new File("imagenes/i3.jpg"));
-            System.out.println("La imagen nI3 se ha guardado correctamente en i3.jpg");
-        } catch (Exception e) {
-            System.err.println("Ocurrio un error al intentar guardar la informacion de la imagen creada nI3 en el nuevo archivo i3.jpg");
-        }
-
-        /* Este bloque de codigo crea una nueva imagen nI4 con las mismas caracteristicas de la imagen i, y 
-         * la modifica pixel por pixel para que nI4 ahora este en tonos sepia. 
-         * 
-         * Crea una nueva imagen nI4 exactamente con las mismas caracteristicas de i, utiliza ciclos anidados para recorrer pixel por 
-         * pixel la imagen y asignarle un nuevo valor rgb en tonos sepia. Por ultimo guarda la imagen creada nI4
-         * en un nuevo archivo formato jpg llamado i4.jpg en el directorio imagenes.
-         */
-
-        BufferedImage nI4 = new BufferedImage(i.getWidth(), i.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        Color c4;
-        int c1, c2a, c3a, c11, c22, c33;
-
-        for (int y = 0; y < i.getHeight(); y++) {
-            for (int x = 0; x < i.getWidth(); x++) {
-
-                c4 = new Color(i.getRGB(x, y));
-
+                // el siguiente codigo es para la modificacion de la imagen 4
                 c1 = c4.getRed();
                 c2a = c4.getGreen();
                 c3a = c4.getBlue();
@@ -133,15 +66,33 @@ public class Practica2 {
                 c22 = (c22 > 255) ? 255 : (c22 < 0) ? 0 : c22; 
                 c33 = (c33 > 255) ? 255 : (c33 < 0) ? 0 : c33; 
 
-                nI4.setRGB(x, y, new Color(c11, c22, c33).getRGB());
+
+                // asignacion de los nuevos valores rgb a los pixeles en las respectivas imagenes
+                nI.setRGB(z, w, new Color(p, p, p).getRGB());
+                nI2.setRGB(z, w, new Color(c2.getRed(), 0, 0).getRGB());
+                nI3.setRGB(z, w, new Color(0, c3.getGreen(), 0).getRGB());
+                nI4.setRGB(z, w, new Color(c11, c22, c33).getRGB());
             }
         }
-
+        
+        /* 
+         * Este codigo guarda la informacion de las imagenes creadas en archivos nuevos en formato jpg, si ocurre un error maneja la excepcion
+         * de manera adecuada
+         */
         try {
+            ImageIO.write(nI, "jpg", new File("imagenes/i1.jpg"));
+            System.out.println("La imagen nI se ha guardado correctamente en i1.jpg");
+            
+            ImageIO.write(nI2, "jpg", new File("imagenes/i2.jpg"));
+            System.out.println("La imagen nI2 se ha guardado correctamente en i2.jpg");
+
+            ImageIO.write(nI3, "jpg", new File("imagenes/i3.jpg"));
+            System.out.println("La imagen nI3 se ha guardado correctamente en i3.jpg");
+
             ImageIO.write(nI4, "jpg", new File("imagenes/i4.jpg"));
             System.out.println("La imagen nI4 se ha guardado correctamente en i4.jpg");
         } catch (Exception e) {
-            System.err.println("Ocurrio un error al intentar guardar la informacion de la imagen creada nI4 en el nuevo archivo i4.jpg");
+            System.err.println("Ocurrio un error al intentar guardar la informacion de las imagenes creadas en los nuevos archivos");
         }
     }
 }
