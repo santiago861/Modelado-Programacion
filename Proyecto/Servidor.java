a7d05adfd69f2efda1ff179b3ab24cc90b6723c1
Santiago Reyes <santiago_reyes@ciencias.unam.mx>
2023-11-15 20:54:41 -0600

    ahora el manejo de cada cliente se hace en un hilo por separado

--- Proyecto/Servidor.java
+++ Proyecto/Servidor.java
@@ -1,8 +1,7 @@
-/**
- * @author Yanez Diaz Carlos
- * @author Reyes Medina Santiago Ivan
- */
-
+// /**
+//  * @author Yanez Diaz Carlos
+//  * @author Reyes Medina Santiago Ivan
+//  */
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -12,44 +11,69 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
-public class Servidor{
+public class Servidor {
     public static void main(String[] args) {
         int port = 7400; // Puerto en el que escuchará el servidor
         ServerSocket serverSocket = null;
-        Socket socketCliente = null;
-        BufferedReader in = null;
-        BufferedWriter out = null;
 
         try {
             serverSocket = new ServerSocket(port);
             System.out.println("Esperando una conexión en el puerto " + port + "...");
 
-            socketCliente = serverSocket.accept();
-            System.out.println("Conexión establecida desde " + socketCliente.getInetAddress() + ":" + socketCliente.getPort());
-
-            in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream()));
-
-            String msjCliente;
-
             while (true) {
-                msjCliente = in.readLine();
-                if (msjCliente == null) {
-                    break; // Si el cliente cierra la conexión, salimos del bucle
-                }
-                System.out.println("Mensaje del cliente: " + msjCliente);
+                Socket socketCliente = serverSocket.accept();
+                System.out.println("Conexión establecida desde " + socketCliente.getInetAddress() + ":" + socketCliente.getPort());
+
+                // Crea un nuevo hilo para manejar la conexión del cliente
+                Thread clientHandlerThread = new Thread(new ClienteHandler(socketCliente));
+                clientHandlerThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (in != null) in.close();
-                if (out != null) out.close();
-                if (socketCliente != null) socketCliente.close();
                 if (serverSocket != null) serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    // Clase interna para manejar la conexión de cada cliente en un hilo separado
+    private static class ClienteHandler implements Runnable {
+        private Socket socketCliente;
+        private BufferedReader in;
+        private BufferedWriter out;
+
+        public ClienteHandler(Socket socketCliente) {
+            this.socketCliente = socketCliente;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream()));
+
+                String msjCliente;
+
+                while (true) {
+                    msjCliente = in.readLine();
+                    if (msjCliente == null) {
+                        break; // Si el cliente cierra la conexión, salimos del bucle
+                    }
+                    System.out.println("Mensaje del cliente: " + msjCliente);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (in != null) in.close();
+                    if (out != null) out.close();
+                    if (socketCliente != null) socketCliente.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
