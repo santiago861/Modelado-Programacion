4b09eaa03fce1dd26574a55d38e96e0d984013e9
Carlos Yañez <carlos.yd@ciencias.unam.mx>
2023-11-19 20:40:40 -0600

    Se actualizaron algunas funciones del proyecto

--- Proyecto/Cliente.java
+++ Proyecto/Cliente.java
@@ -4,50 +4,54 @@
  */
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
+ import java.io.BufferedReader;
+ import java.io.BufferedWriter;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.io.OutputStreamWriter;
+ import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+ public class Cliente{
+     static Scanner entrada = new Scanner(System.in);
+     public static void main(String[] args) {
+         String serverHost = "127.0.0.1";
+         int serverPort = 7400; // Puerto del servidor
+         Socket socket = null;
+         BufferedReader msj = null;
+         BufferedReader user = null;
+         BufferedWriter out = null;
 
-public class Cliente{
-    public static void main(String[] args) {
-        String serverHost = "127.0.0.1";
-        int serverPort = 7400; // Puerto del servidor
-        Socket socket = null;
-        BufferedReader msj = null;
-        BufferedWriter out = null;
-
-        try {
-            socket = new Socket(serverHost, serverPort);
-            System.out.println("Conectado al servidor...");
-
-            msj = new BufferedReader(new InputStreamReader(System.in));
-            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-            String cliente;
-
-            System.out.println("Escribe un mensaje (o 'salir' para salir):");
-
-            while (true) {
-                cliente = msj.readLine();
-                if (cliente == null || cliente.equalsIgnoreCase("salir")) {
-                    break; // Salir del bucle si el usuario escribe salir
-                }
-                out.write(cliente + "\n");
-                out.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (out != null) out.close();
-                if (msj != null) msj.close();
-                if (socket != null) socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+         try {
+             socket = new Socket(serverHost, serverPort);
+             System.out.println("Conectado al servidor...");
+             msj = new BufferedReader(new InputStreamReader(System.in));
+             user = new BufferedReader(new InputStreamReader(System.in));
+             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             System.out.println("");
+             System.out.println("Ingresa tu nombre de usuario para entrar al servidor: ");
+             
+             String cliente;
+             while (true) {
+                 cliente = msj.readLine();
+                 System.out.println("Escribe tu mensaje: ");
+                 if (cliente == null || cliente.equalsIgnoreCase("salir")) {
+                     break; // Salir del bucle si el usuario escribe salir
+                 }
+                 out.write(cliente + "\n");
+                 out.flush();
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         } finally {
+             try {
+                 if (out != null) out.close();
+                 if (msj != null) msj.close();
+                 if (socket != null) socket.close();
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }
+     }
+ }
--- Proyecto/Servidor.java
+++ Proyecto/Servidor.java
@@ -10,12 +10,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Servidor {
-    public static void main(String[] args) {
+
+    static ArrayList<String> users = new ArrayList<>();
+      public static void main(String[] args) {
         int port = 7400; // Puerto en el que escuchará el servidor
         ServerSocket serverSocket = null;
-
+        
         try {
             serverSocket = new ServerSocket(port);
             System.out.println("Esperando una conexión en el puerto " + port + "...");
@@ -23,7 +26,6 @@ public class Servidor {
             while (true) {
                 Socket socketCliente = serverSocket.accept();
                 System.out.println("Conexión establecida desde " + socketCliente.getInetAddress() + ":" + socketCliente.getPort());
-
                 // Crea un nuevo hilo para manejar la conexión del cliente
                 Thread clientHandlerThread = new Thread(new ClienteHandler(socketCliente));
                 clientHandlerThread.start();
@@ -42,7 +44,8 @@ public class Servidor {
     // Clase interna para manejar la conexión de cada cliente en un hilo separado
     private static class ClienteHandler implements Runnable {
         private Socket socketCliente;
-        private BufferedReader in;
+        private BufferedReader inMsj;
+        private BufferedReader inUser;
         private BufferedWriter out;
 
         public ClienteHandler(Socket socketCliente) {
@@ -52,23 +55,25 @@ public class Servidor {
         @Override
         public void run() {
             try {
-                in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                inMsj = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                inUser = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
                 out = new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream()));
-
+                String usuario; 
                 String msjCliente;
-
+                usuario = inUser.readLine();
+                users.add(usuario);
                 while (true) {
-                    msjCliente = in.readLine();
+                    msjCliente = inMsj.readLine();
                     if (msjCliente == null) {
                         break; // Si el cliente cierra la conexión, salimos del bucle
                     }
-                    System.out.println("Mensaje del cliente: " + msjCliente);
+                    System.out.println("Mensaje de "+ usuario + ":  "  + msjCliente);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    if (in != null) in.close();
+                    if (inMsj != null) inMsj.close();
                     if (out != null) out.close();
                     if (socketCliente != null) socketCliente.close();
                 } catch (IOException e) {
