d1cfbcb28e8e844282086e1ea8ada30a939df932
Santiago Reyes <santiago_reyes@ciencias.unam.mx>
2023-11-23 22:01:25 -0600

    se agregaron buenas practicas

---
+++ Proyecto/Constantes.java
@@ -0,0 +1,8 @@
+public abstract class Constantes {
+
+	public static final int PUERTO_SERVIDOR = 42455;
+	
+	public static final String CODIGO_SALIDA = "/salir";
+	public static final String CODIGO_LISTAR = "/listar";
+	public static final String CODIGO_ACTUALIZAR_CONECTADOS = "/actualizarConectados";
+}
